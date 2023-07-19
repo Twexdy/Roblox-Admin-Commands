fa87652dac76f38de014489b1f6e905b1d511511
Twexdy <119236284+Twexdy@users.noreply.github.com>
2023-07-19 16:45:16 -0600

    Removed some commands for easier testing.

--- src/ServerStorage/ServerCommands/god.lua
+++
@@ -1,9 +0,0 @@
--- argumentsGiven[1] = manyPlayers
-
-return function(argumentsGiven)
-	local players = argumentsGiven[1]
-	for _, v in pairs(players) do
-		v.Character:WaitForChild("Humanoid").MaxHealth = math.huge
-		v.Character:WaitForChild("Humanoid").Health = math.huge
-	end
-end
